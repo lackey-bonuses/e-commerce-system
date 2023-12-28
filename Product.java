b7d86d2d421296e2dfbd6f66fbba899785c75444
Robert B <152427188+lackey-bonuses@users.noreply.github.com>
2023-12-28 17:34:28 +0600

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,29 @@
+public class Product {
+    private int productId;
+    private String productName;
+    private double price;
+
+    public Product(int productId, String productName, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayProductInfo() {
+        System.out.println("Product ID: " + productId);
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: $" + price);
+    }
+}
